64682887e7b7301175f2f05a661f67fb5c37dd8d
VegasAndroid <vegaslancer1025@gmail.com>
2023-03-10 05:09:30 +0900

    Update the index page, big button

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,22 +4,56 @@
 <script src="{{asset('js/dashboard.js')}}"></script>
 @endsection
 @section('content')
-<h1>Hello</h1>
+<style>
+    .big-btn-a {
+        display: flex;
+        justify-content: center
+    }
+    .big-btn {
+        background-color: green; 
+        border-radius: 50%; 
+        width: 200px; 
+        height: 200px; 
+        display: flex; 
+        justify-content: center; 
+        align-items: center;
+        margin: 20px;
+        color: white;
+        font-size: 25px;
+    }
+
+</style>
 <section class="content">
     <div class="container-fluid">
         <!-- Main row -->
         <div class="row mt-3">
             <div class="col-xs-12 col-sm-12 col-md-3">
-                <a class="btn btn-success" href="{{ route('datacollection') }}"> Collect Data</a>
+                <a class="big-btn-a" href="{{ route('datacollection') }}">
+                    <div class="big-btn">
+                        Collect Data
+                    </div>
+                </a>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-3">
-                <a class="btn btn-success" href="{{ route('datacollection') }}"> Sorting</a>
+                <a class="big-btn-a" href="{{ route('datacollection') }}"> 
+                    <div class="big-btn">
+                        Sorting
+                    </div>
+                </a>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-3">
-                <a class="btn btn-success" href="{{ route('datacollection') }}"> Reports</a>
+                <a class="big-btn-a" href="{{ route('datacollection') }}">
+                    <div class="big-btn">
+                        Sorting Reports
+                    </div>
+                </a>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-3">
-                <a class="btn btn-success" href="{{ route('datacollection') }}"> Hello</a>
+                <a class="big-btn-a" href="{{ route('datacollection') }}"> 
+                    <div class="big-btn">
+                        Hello
+                    </div>
+                </a>
             </div>
         </div>
     </div>
